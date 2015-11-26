9ea9a0cb03787e37cdf46606b4ad52cc6d085bca
andychong1996 <andygg1996personal@gmail.com>
2015-11-26 17:14:48 +0800

    added modals for login, logout, forgot password, and rerouted links in the modals, deleted padding-top for all sections to fix background overflow for some screen

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'dashboard', to: 'users#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
+    # devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
+
+  devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations",omniauth_callbacks: "callbacks" }
   get 'home/index'
 
   root 'home#index'
-  
+
 end
