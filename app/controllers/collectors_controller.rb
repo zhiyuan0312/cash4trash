60b7a3f9e4df967a96a07c793d3ac2a63c433b7f
zhiyuan0312 <zhiyuan0312@hotmail.com>
2015-12-01 15:35:45 +0800

    updated job status model

--- app/controllers/collectors_controller.rb
+++ app/controllers/collectors_controller.rb
@@ -5,6 +5,18 @@ class CollectorsController < ApplicationController
 
   def index
   	@collector = current_collector
-  	@existing_schedules = Schedule.where(:collector_id == nil)
+  	@unpicked_schedules = Schedule.where(collector_id: nil)
+  	@owned_schedules = Schedule.where(collector_id: current_collector.id)
+  	@incomplete_jobs = []
+  	@completed_jobs = []
+  	@owned_schedules.each do |s|
+  		if s.collection == nil
+  			@incomplete_jobs << s
+  		else 
+  			@completed_jobs << s
+  		end
+  	end
+
+  	byebug
   end
 end
