085a08a6ef14c2b43c56af63629eb78a94704e3d
Chai Wei Jun <chaiweijun@Chais-Mac.local>
2015-12-02 19:13:51 +0800

    completed js fixes for turbolinks

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,35 +11,6 @@ $(document).on('page:load', function(){
       }
     });
   });
-      
-  $('#renew-password-link1').click(function(){
-    $('#loginModal').modal('hide');
-    setTimeout(function(){
-      $('#renew-passwordModal').modal('show');
-    }, 100);
-  });
-
-  $('#renew-password-link2').click(function(){
-    $('#signupModal').modal('hide');
-    setTimeout(function(){
-      $('#renew-passwordModal').modal('show');
-    }, 100);
-  });
-
-  $('#sign-in-link1').click(function(){
-    $('#signupModal').modal('hide');
-    setTimeout(function(){
-      $('#loginModal').modal('show');
-    }, 100);
-  });
-
-  $('#sign-up-link1').click(function(){
-    $('#loginModal').modal('hide');
-    setTimeout(function(){
-      $('#signupModal').modal('show');
-    }, 100);
-
-  });
 
 });
 
